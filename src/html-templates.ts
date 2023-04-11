24630c3a62b4e4b87c663ef1f78387cc15cc1eb6
mariohamann <mario.hamann@virtual-identity.com>
2023-04-11 16:03:38 +0200

    feat: improve html-templates

--- src/html-templates.ts
+++ src/html-templates.ts
@@ -29,28 +29,24 @@ export function getTemplate(
   const slotsTemplate = getSlotsTemplate(component!, args);
   syncControls(component!);
 
-  return html`
-      ${getStyleTemplate(component, args)}
-      <${unsafeStatic(component!.tagName!)} ${spread(operators)}>
-        ${slotsTemplate}
-        ${slot || ""}
-      </${unsafeStatic(component!.tagName!)}>
-      <script>
-        component = document.querySelector('${component!.tagName!}');
-      </script>
-    `;
+  return html`${getStyleTemplate(component, args)}
+<${unsafeStatic(component!.tagName!)} ${spread(operators)}>
+  ${slotsTemplate}
+  ${slot || ""}
+</${unsafeStatic(component!.tagName!)}>
+
+<script>
+  component = document.querySelector('${component!.tagName!}');
+</script>
+`;
 }
 
 export function getStyleTemplate(component?: Declaration, args?: any) {
   const cssPropertiesTemplate = getCssPropTemplate(component!, args);
   const cssPartsTemplate = getCssPartsTemplate(component!, args);
 
-  return html`
-    <style>
-      ${cssPropertiesTemplate}
-      ${cssPartsTemplate}
-    </style>
-  `;
+  return cssPropertiesTemplate || cssPartsTemplate ? html`<style>${cssPropertiesTemplate}${cssPartsTemplate}</style>
+` : '';
 }
 
 function getTemplateOperators(component: Declaration, args: any) {
@@ -90,16 +86,27 @@ function getTemplateOperators(component: Declaration, args: any) {
 function getCssPropTemplate(component: Declaration, args: any) {
   const cssProperties = getCssProperties(component);
 
+  const hasCssProperties = Object.keys(cssProperties).some((key) => {
+    const cssValue = args![key];
+    return cssValue ? true : false;
+  });
+
+  if (!hasCssProperties) {
+    return;
+  }
+
   const template = unsafeStatic(`
-      ${component?.tagName} {
-        ${Object.keys(cssProperties)
-          .map((key) => {
-            const cssName = cssProperties[key].name;
-            const cssValue = args![key];
-            return cssValue ? `${cssName}: ${cssValue || ""};` : "";
-          })
-          .join("\n")}
-      }`);
+  ${component?.tagName} {
+    ${Object.keys(cssProperties)
+      .map((key) => {
+        const cssName = cssProperties[key].name;
+        const cssValue = args![key];
+        return cssValue ? `${cssName}: ${cssValue || ""};` : null;
+      })
+      .filter((value) => value !== null)
+      .join("\n    ")}
+  }
+`);
 
   return template;
 }
@@ -107,17 +114,29 @@ function getCssPropTemplate(component: Declaration, args: any) {
 function getCssPartsTemplate(component: Declaration, args: any) {
   const cssParts = getCssParts(component);
 
+  const hasCssParts = Object.keys(cssParts).some((key) => {
+    const cssValue = args![key];
+    return cssValue ? true : false;
+  });
+
+  if (!hasCssParts) {
+    return;
+  }
+
   const template = unsafeStatic(
     `${Object.keys(cssParts)
       .filter((key) => key.endsWith("-part"))
       .map((key) => {
         const cssPartName = cssParts[key].name;
         const cssPartValue = args![key];
-        return `${component?.tagName}::part(${cssPartName}) {
-          ${cssPartValue || ""}
-        }`;
+        return cssPartValue ? `
+  ${component?.tagName}::part(${cssPartName}) {
+    ${cssPartValue || ""}
+  }` : null;
       })
-      .join("\n")}`
+      .filter((value) => value !== null)
+      .join("\n")}
+`
   );
 
   return template;
@@ -137,8 +156,9 @@ function getSlotsTemplate(component: Declaration, args: any) {
           ? slotName === "default"
             ? `${slotValue || ""}`
             : `<div slot="${slotName}">${slotValue || ""}</div>`
-          : "";
+          : null;
       })
+      .filter((value) => value !== null)
       .join("\n")}`
   );
 
